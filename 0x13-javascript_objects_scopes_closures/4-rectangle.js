bb1d5b1c76674c8acb212a55c45fbd8efe67ac5a
Ogu Johnkennedy <ogujohnkennedy@gmail.com>
2022-11-29 17:09:10 -0800

    task 7 to 13

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,24 +1,28 @@
 #!/usr/bin/node
-
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
+
   print () {
-    for (let col = 0; col < this.height; col += 1) {
-      console.log('X'.repeat(this.width));
+    for (let i = 0; i < this.height; i++) {
+      const temp = [];
+      for (let j = 0; j < this.width; j++) temp.push('X');
+      console.log(`${temp.join('')}`);
     }
   }
-  rotate () {
-    let tmp = this.width;
-    this.width = this.height;
-    this.height = tmp;
-  }
+
   double () {
     this.width *= 2;
     this.height *= 2;
   }
-};
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+}
+
+module.exports = Rectangle;
