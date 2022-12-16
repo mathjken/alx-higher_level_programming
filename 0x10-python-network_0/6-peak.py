481a1bcb85500bca15674bceaa5039f93aedf46a
Ogu Johnkennedy <ogujohnkennedy@gmail.com>
2022-12-15 19:42:10 -0800

    corrected 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
-"""
-Function that finds a peak in a list of unsorted integers
+""" This module returns the peak of the list
 """
 
 
 def find_peak(list_of_integers):
+    """ This function returns the peak of the list
     """
-    finds num that's greater than both left and right
-    """
-    if len(list_of_integers) == 0:
+    if (len(list_of_integers) == 0):
         return None
 
-    l = list_of_integers
-    beg = 0
-    end = len(l)-1
-
-    if l[beg] > l[beg+1]:
-        return l[beg]
-    if l[end] > l[end-1]:
-        return l[end]
-
-    mid = (beg+end)//2
-    if l[mid-1] < l[mid] and l[mid+1] < l[mid]:
-        return l[mid]
-    if l[mid] < l[mid-1]:
-        return find_peak(l[beg:mid+1])
-    elif l[mid] < l[mid+1]:
-        return find_peak(l[mid:end+1])
     else:
-        return l[beg]
+        peak = list_of_integers[0]
+        for i in range(len(list_of_integers)):
+            if list_of_integers[i] > peak:
+                peak = list_of_integers[i]
+        return peak
