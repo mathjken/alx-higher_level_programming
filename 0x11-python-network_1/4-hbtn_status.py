0f57020317348502516b6e3c0281500bb6ea2ee2
Ogu Johnkennedy <ogujohnkennedy@gmail.com>
2022-12-19 02:56:36 -0800

    corrected task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""Python script that fetches https://intranet.hbtn.io/status, using
- the package requests
-"""
-
-import requests
+"""Fetches https://alx-intranet.hbtn.io/status"""
 
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    import requests
+
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type:', type(response.content.decode()))
+    print('\t- content:', response.content.decode())
